1b77e2839c2ae18acf45eaa82466b66d27214b6c
Zubin <me@zubin.org.in>
2023-03-25 19:14:34 +0530

    Log v2

--- log.js
+++ log.js
@@ -7,14 +7,16 @@
  * @license MIT
  */
 
-// Log
-
 /*
       PLUGIN CODE
 
 */
 
 async function log(ctx, next) {
+  ctx.config = {
+    logChannel: process.env.CHANNEL_ID,
+  };
+
   const from = ctx.from;
   const name =
     from.last_name === undefined
@@ -23,7 +25,26 @@ async function log(ctx, next) {
   console.log(
     `From: ${name} (@${from.username}) ID: ${from.id}\nMessage: ${ctx.message.text}`
   );
+
+  // Log
+
+  if (
+    ctx.message &&
+    typeof ctx.message.text !== "undefined" &&
+    !ctx.message.text.includes("/")
+  ) {
+    await bot.api.sendMessage(
+      ctx.config.logChannel,
+      `<b>From: ${ctx.from.first_name} (@${from.username}) ID: <code>${from.id}</code></b>`,
+      { parse_mode: "HTML" }
+    );
+
+    await ctx.api.forwardMessage(
+      ctx.config.logChannel,
+      ctx.chat.id,
+      ctx.message.message_id
+    );
+  }
+
   await next();
 }
-
-bot.use(log);
